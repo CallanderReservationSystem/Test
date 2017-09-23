0cb09665414d296b3c7831c57fe2ffe3bfffb266
hsagga <hsagga@calstatela.edu>
2017-09-23 16:00:39 -0700

    mod on test.java

--- src/test/test.java
+++ src/test/test.java
@@ -15,7 +15,7 @@ public class test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
-     * @see HttpServlet#HttpServlet()
+     * see HttpServlet#HttpServlet()
      */
     public test() {
         super();
