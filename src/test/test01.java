f04212be034d24a9ffa6cc47b72e51761c5baf8f
s187 <hsagga@calstatela.edu>
2017-09-27 00:00:24 -0700

    deleted couple of lines

--- src/test/test01.java
+++ src/test/test01.java
@@ -11,10 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 public class test01 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public test01() {
-		super();
-	}
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect("Testing.jsp");
