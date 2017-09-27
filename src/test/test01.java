6715b3fcf29657cc60980b3ffc4b578aa4c540d9
hsagga <hsagga@calstatela.edu>
2017-09-26 23:55:08 -0700

    testing...

---
+++ src/test/test01.java
@@ -0,0 +1,28 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test01")
+public class test01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public test01() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("Testing.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
